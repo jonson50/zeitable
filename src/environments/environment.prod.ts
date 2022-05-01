67ab06a8f9f906a2df5e8cffd84ab7cc97378e99
John Bermeo <john.bermeo@gmail.com>
2022-05-01 22:51:25 +0200

    basic dark-mode added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://parseapi.back4app.com/parse',
+  APP_ID: 's6Xd0M5aEW7v0mBH75Tpr0KttYCeWJLO8WYJeF2Z',
+  API_KEY: 'CjXafIqxmr9Omc6UBhEHf8LuL96YlY3VziY76N6j',
+  DEC: 'u7x!A%D*F-JaNdRg',
 };
