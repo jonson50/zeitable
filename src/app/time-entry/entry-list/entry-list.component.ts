133c1df0e45508c90e7068918671104030497ca4
John Bermeo <john.bermeo@gmail.com>
2022-07-22 19:19:25 +0200

    missing entry-list.component.ts not commited previously

--- src/app/time-entry/entry-list/entry-list.component.ts
+++ src/app/time-entry/entry-list/entry-list.component.ts
@@ -6,18 +6,11 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./entry-list.component.scss'],
 })
 export class EntryListComponent implements OnInit {
-<<<<<<< HEAD
   daysInMonth: Date[] = [];
   currentDate!: Date;
-=======
-   daysName = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
-   currentDate = new Date();
-   entryData:any = [];
->>>>>>> 50b1d5cdcd6c19f62bf3e3d8c31a8a55281409e0
 
    constructor() {}
 
-<<<<<<< HEAD
   ngOnInit(): void {
     this.currentDate = new Date();
     this.daysInMonth = this.getDaysInMonth(this.currentDate);
@@ -46,42 +39,4 @@ export class EntryListComponent implements OnInit {
     this.currentDate = new Date(this.currentDate);
     this.daysInMonth = this.getDaysInMonth(this.currentDate);
   }
-=======
-   ngOnInit(): void {
-      this.loadEntryData();
-   }
-
-   loadEntryData() {
-      let monthDays = this.getDaysInMonth(this.currentDate.getMonth(), this.currentDate.getFullYear())
-      console.log(monthDays)
-      monthDays.forEach(date => {
-         let data = {
-            date: date,
-            day: date.getDay(),
-            description: `Day's description`,
-            should: 0,
-            is: 0,
-            active: true,
-            closed: false,
-            records: []
-         }
-         this.entryData.push(data);
-      });
-   }
-
-   /*
-   *  @param   {int} Month number, 0 based
-   *  @param   {int} year, not zero based, required to account for leap years
-   *  @return  {Date[]} List with date objects for each day of the month
-   */
-   getDaysInMonth(mount:number, year:number): Date[] {
-      let date = new Date(year, mount, 1);
-      let days = [];
-      while(date.getMonth() === mount) {
-         days.push(new Date(date));
-         date.setDate(date.getDate() + 1);
-      }
-      return days;
-   }
->>>>>>> 50b1d5cdcd6c19f62bf3e3d8c31a8a55281409e0
 }
